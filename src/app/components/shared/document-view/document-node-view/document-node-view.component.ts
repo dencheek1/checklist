eec95edb63619300699274a0e3f30722864d39a6
zatyupo_ds <zatyupo_ds@VITEBSK.BELTELECOM.BY>
2024-09-02 16:36:31 +0300

    do something

--- src/app/components/shared/document-view/document-node-view/document-node-view.component.ts
+++ src/app/components/shared/document-view/document-node-view/document-node-view.component.ts
@@ -1,7 +1,14 @@
-import { Component, ComponentRef, inject, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  ComponentRef,
+  ElementRef,
+  inject,
+  Input,
+  OnInit,
+} from '@angular/core';
 import { DocumentNode } from '../../../../core/document-node';
 import { DocumentViewComponent } from '../document-view.component';
-import { DocumentTableViewComponent } from "../document-table-view/document-table-view.component";
+import { DocumentTableViewComponent } from '../document-table-view/document-table-view.component';
 import { PreviewService } from '../../../../service/preview/preview.service';
 
 @Component({
@@ -11,13 +18,43 @@ import { PreviewService } from '../../../../service/preview/preview.service';
   templateUrl: './document-node-view.component.html',
   styleUrl: './document-node-view.component.scss',
 })
-export class DocumentNodeViewComponent implements OnInit{
+export class DocumentNodeViewComponent implements OnInit {
   private prs = inject(PreviewService);
   @Input() node: DocumentNode = {} as DocumentNode;
 
-  leafClass = this.node.properties; 
- ngOnInit(): void {
-  this.leafClass = 
-   console.log(this.leafClass);
- }
+  leafClass = this.node.properties;
+  removeVisibilityLevel = 'visibility-0';
+
+  ngOnInit(): void {
+    console.log(this.leafClass);
+  }
+
+  hover(e: Event) {
+    console.log((e.currentTarget as HTMLElement).getBoundingClientRect());
+    console.log((e as MouseEvent).offsetX);
+    e.stopPropagation();
+    this.removeVisibilityLevel = 'visibility-1';
+
+    let boundingRect = (e.currentTarget as HTMLElement).getBoundingClientRect();
+    let mousePositionX = (e as MouseEvent).x;
+    let ratio = (mousePositionX - boundingRect.x)/ boundingRect.width;
+
+    console.log(ratio)
+    
+    if (ratio < 0.25 && e.bubbles) {
+      this.removeVisibilityLevel = 'visibility-1';
+    } else if (ratio < 0.5 && e.bubbles) {
+      this.removeVisibilityLevel = 'visibility-1';
+    } else if (ratio < 0.75 && e.bubbles){
+      this.removeVisibilityLevel = 'visibility-2';
+    }
+    else{
+      this.removeVisibilityLevel = 'visibility-3';
+    }
+  }
+
+  mouseLeave(){
+    this.removeVisibilityLevel = 'visibility-0';
+  }
+
 }
